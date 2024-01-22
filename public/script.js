6a8b74407b92fd29a011a248f2588dc4c998a06a
TatevMargaryan0331 <tatev.margaryan1.y@tumo.org>
2024-01-22 21:32:09 +0400

    5 lesson

--- public/script.js
+++ public/script.js
@@ -12,13 +12,14 @@
 
 function SendData(){
     let nameInput = document.getElementById("name").value
-    let ageInput = document.getElementById("age").value
+    let passwordInput = document.getElementById("password").value
+    let emailInput = document.getElementById("email").value
 
     fetch('/addName', {
         method: "POST",
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
-        body: JSON.stringify({name : nameInput, age : ageInput})
+        body: JSON.stringify({name : nameInput, password : passwordInput, email : emailInput})
     });
 }
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var path = require("path");
 const bodyParser = require("body-parser");
 var app = express();
 
+const mongoose = require('mongoose');
+const connectionString = 'mongodb+srv://TatevMargaryan0331:saymynameMao17@cluster0.s13geds.mongodb.net/sample_mflix';
+// mongoose.connect(connectionString);
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Connection error:'));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -27,71 +33,31 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/form.html"));
 });
 
-// app.post("/addName", (req, res) => {
-//     const name = req.body.name;
-//     const age = req.body.age;
-//     console.log("Received data: ", name, age);
-//     //res.status(200).send("Data received successfully");
-//     res.redirect("/");
-// });
-
 app.listen(3000, function() {
     console.log("Example is running on port 3000");
 });
 
-// const mongoose = require('mongoose');
+app.post("/addName", async(req, res) => {
+  const name = req.body.name;
+  const password = req.body.password;
+  const email = req.body.email;
+  mongoose.connect(connectionString);
+  const db = mongoose.connection;
+  db.once('open', async () => {
+    console.log('Connected to MongoDB! ');
+    try {
+      await db.collection('users').insertOne({
+        "name": name,
+        "email": email,
+        "password": password
+      }); 
+    } catch (error) {
+      console.error('Error retrieving data:', error);
+    } finally {
+      // Make sure to close the connection when you're done
+      mongoose.connection.close();
+    }
+  });  
 
-// // Replace the connection string with your MongoDB connection string
-// const connectionString = 'mongodb+srv://TatevMargaryan0331:saymynameMao17@cluster0.s13geds.mongodb.net/?retryWrites=true&w=majority';
-
-
-// // Connect to MongoDB
-// mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// // Check the connection
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'Connection error:'));
-// db.once('open', () => {
-// console.log('Connected to MongoDB!');
-// // You can add additional code here for testing or other operations
-// // Make sure to close the connection when you're done
-// mongoose.connection.close();
-// });
-
-const mongoose = require('mongoose');
-const { log } = require("console");
-
-// Define a Mongoose schema and model for the Person collection
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
 });
 
-const Person = mongoose.model('Person', personSchema);
-
-// Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://TatevMargaryan0331:saymynameMao17@cluster0.s13geds.mongodb.net/?retryWrites=true&w=majority';
-
-// Connect to MongoDB
-mongoose.connect(connectionString);
-
-// Check the connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', async () => {
-  console.log('Connected to MongoDB!');
-
-  try {
-    // Retrieve data from the Person collection
-    const people = await Person.find(); // Retrieve all documents
-    //console.log(db);
-    //console.log(db.Collection);
-    const result = mongoose.connection.db.collection("sample_mflix").find.toArray();
-    console.log('Retrieved data:', result);
-  } catch (error) {
-    console.error('Error retrieving data:', error);
-  } finally {
-    // Make sure to close the connection when you're done
-    mongoose.connection.close();
-  }
-});
