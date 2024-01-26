729b0ce531bbe52aa88e16e0ac55cfa79cb4463f
TatevMargaryan0331 <tatev.margaryan1.y@tumo.org>
2024-01-26 21:26:36 +0400

    lesson 6

--- server.js
+++ server.js
@@ -5,32 +5,62 @@ var app = express();
 
 const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://TatevMargaryan0331:saymynameMao17@cluster0.s13geds.mongodb.net/sample_mflix';
-// mongoose.connect(connectionString);
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'Connection error:'));
+mongoose.connect(connectionString);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+// db.once('open', async () => {
+//   console.log('Connected to MongoDB! ');
+//   try {
+//     // await db.collection('movies').updateOne({title : "Street Angel"}, {$set: {year : 2020}})
+
+//     // var filter = {title : "Street Angel"};
+//     // var update = {$set: {year : 2020}};
+//     // await db.collection('movies').findOneAndUpdate(filter,update)//,
+//     //   {
+//     //   new: true
+//     // });
+
+//     // var filter = {title : "Street Angel"};
+//     // var update = {$set: {
+//     //   runtime : 222,
+//     //   plot : "My dear Angel",
+//     //   countries : ["Germany"]
+//     // }};
+//     // await db.collection('movies').findOneAndUpdate(filter,update)
+
+//     // await db.collection('users').deleteOne({name : "Robert Baratheon"})
+
+
+//   } catch (error) {
+//     console.error('Error retrieving data:', error);
+//   } finally {
+//     // Make sure to close the connection when you're done
+//     mongoose.connection.close();
+//   }
+// });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
-app.param('team_id', function(req, res, next, team_id) {
 
-    Team.findOne(team_id, function(err, team) {
+app.get("/", async function(req, res) {
+    // res.sendFile(path.join(__dirname, "./public/form.html"));
+    var info = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
 
-        if (err) {
-            return next(err);
-        }
-        if (!team) {
-            throw new Error("no team matched");
-        }
-        req.team = team;
-        next();
-    });
-});
+    mongoose.connect(connectionString);
+    const db = mongoose.connection;
+    let theatersArr = await db.collection('theaters').find({"location.address.city" : "Bloomington"}).toArray();
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/form.html"));
+    res.render('../public/form.ejs', {
+      theatersArr : theatersArr
+  });
 });
 
 app.listen(3000, function() {
@@ -50,14 +80,13 @@ app.post("/addName", async(req, res) => {
         "name": name,
         "email": email,
         "password": password
-      }); 
+      });
     } catch (error) {
       console.error('Error retrieving data:', error);
     } finally {
       // Make sure to close the connection when you're done
       mongoose.connection.close();
     }
-  });  
+  });
 
 });
-
